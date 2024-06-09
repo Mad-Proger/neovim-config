851a83be93979c88d44010d574b61efe1af7b026
Mikhail Zaytsev <m.zaytsev.23@gmail.com>
2024-06-09 18:52:06 +0300

    disable rust-tools

--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -6,14 +6,16 @@ return {
             vim.g.rustfmt_autosave = 1
         end
     },
-    {
-        "simrat39/rust-tools.nvim",
-        dependencies = { "neovim/nvim-lspconfig" },
-        ft = "rust",
-        opts = true,
-    },
+    -- {
+    --     "simrat39/rust-tools.nvim",
+    --     dependencies = { "neovim/nvim-lspconfig" },
+    --     ft = "rust",
+    --     opts = true,
+    -- },
     {
         "mrcjkb/rustaceanvim",
+        version = "^4",
+        lazy = false,
         ft = "rust",
     },
 }
