c219525e830f97fbff6361d02b42311c1965317e
Mikhail Zaytsev <m.zaytsev.23@gmail.com>
2024-06-09 19:14:57 +0300

    remove redundant plugins

--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -1,18 +1,5 @@
 return {
     {
-        "rust-lang/rust.vim",
-        ft = "rust",
-        init = function()
-            vim.g.rustfmt_autosave = 1
-        end
-    },
-    -- {
-    --     "simrat39/rust-tools.nvim",
-    --     dependencies = { "neovim/nvim-lspconfig" },
-    --     ft = "rust",
-    --     opts = true,
-    -- },
-    {
         "mrcjkb/rustaceanvim",
         version = "^4",
         lazy = false,
