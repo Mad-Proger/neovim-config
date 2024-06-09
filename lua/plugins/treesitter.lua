a31b10c1f48b4306650ccbe588639e0ae7b71f72
Mikhail Zaytsev <m.zaytsev.23@gmail.com>
2024-06-09 19:36:16 +0300

    fix treesitter main

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,8 +2,10 @@ return {
     {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
+        main = "nvim-treesitter.configs",
         opts = {
             ensure_installed = { "lua", "rust", "python", "cpp", "cmake" },
+            sync_install = true,
             auto_install = true,
             highlight = { enable = true },
             indent = { enable = true },
