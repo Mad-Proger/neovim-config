074e06eefe9e6692d7aeb5cc0ef28fed9859315f
Mikhail Zaytsev <m.zaytsev.23@gmail.com>
2024-06-15 22:06:45 +0300

    add vimdoc requirement

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
         build = ":TSUpdate",
         main = "nvim-treesitter.configs",
         opts = {
-            ensure_installed = { "lua", "rust", "python", "cpp", "cmake" },
+            ensure_installed = { "lua", "rust", "python", "cpp", "cmake", "vimdoc" },
             highlight = { enable = true },
             indent = { enable = true },
         },
