10f558e25fdc1c411fb9184ae52b19c014f18f7d
Mikhail Zaytsev <m.zaytsev.23@gmail.com>
2024-06-21 16:45:37 +0300

    add basic debugger keymaps

---
+++ lua/keymaps/debug.lua
@@ -0,0 +1,25 @@
+local dap = require("dap")
+
+return {
+    {
+        keys = "<F9>",
+        command = dap.toggle_breakpoint,
+    },
+    {
+        keys = "<F8>",
+        command = dap.step_over,
+    },
+    {
+        keys = "<F7>",
+        command = dap.step_into,
+    },
+    {
+        -- Shift + F7
+        keys = "<F19>",
+        command = dap.step_out,
+    },
+    {
+        keys = "<F5>",
+        command = dap.continue,
+    }
+}
