69a4bd614ea5220f38571cd791bb86d9382a9212
Mikhail Zaytsev <m.zaytsev.23@gmail.com>
2024-07-13 13:09:03 +0300

    change default cmake generator to ninja

--- lua/plugins/cmake.lua
+++ lua/plugins/cmake.lua
@@ -6,8 +6,8 @@ return {
         cmake_generate_options = {
             "-DCMAKE_EXPORT_COMPILE_COMMANDS=1",
             "-DCMAKE_INSTALL_PREFIX=./install",
+            "-GNinja",
         },
-        cmake_build_options = { string.format("-j %d", #vim.loop.cpu_info()) },
         cmake_build_directory = "build/${variant:buildType}",
         cmake_dap_configuration = {
             name = "cpp",
