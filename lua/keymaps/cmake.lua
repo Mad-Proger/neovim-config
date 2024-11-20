c32f1055b002fe36213174f9bba87cb392b07fbe
Mikhail Zaytsev <m.zaytsev.23@gmail.com>
2024-11-20 09:39:11 +0300

    add cmake build keymap

--- lua/keymaps/cmake.lua
+++ lua/keymaps/cmake.lua
@@ -4,5 +4,10 @@ return {
         keys = "<F29>",
         command = ":CMakeDebug<CR>",
         opts = { silent = true },
+    },
+    {
+        keys = "<F17>",
+        command = ":CMakeBuild<CR>",
+        opts = { silent = true },
     }
 }
